ed44abb6dbf0fad0ffff039f3ca1b9a8881b0ab8
geteroseksuAll <Shvagin04@mail.ru>
2024-01-16 09:36:57 +0300

    add deploy.sh, chane vue.config.js

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+ git push -f git@github.com:<geteroseksuAll>/<chem-base>.git master:gh-pages
+
+cd -
