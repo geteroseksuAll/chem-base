3ca9d6dabdf26a4f632240da36657c69e83b3e0c
geteroseksuAll <Shvagin04@mail.ru>
2024-02-01 12:47:28 +0300

    added links of items block

--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -7,15 +7,24 @@
         </p>
         <p class="catalog_block_header_text" v-else>Каталог продукции</p>
       </div>
-      <div
-        class="proverka"
-        v-for="item in this.$store.getters.getSearchList"
-        :key="item.id"
-      >
-        <router-link :to="'/catalog/' + item.id">
-          {{ item.name }}<br />
-          {{ item.casNumber }}<br
-        /></router-link>
+      <div class="catalog_block_items">
+        <ul class="catalog_block_content_links">
+          <li class="block_content_link">Органические соединения</li>
+          <li class="block_content_link">Органические соединения</li>
+          <li class="block_content_link">Органические соединения</li>
+          <li class="block_content_link">Органические соединения</li>
+          <li class="block_content_link">Органические соединения</li>
+          <li class="block_content_link">Органические соединения</li>
+          <li class="block_content_link">Органические соединения</li>
+          <li class="block_content_link">Органические соединения</li>
+        </ul>
+        <div
+          class="catalog_block_content_items-list"
+          v-for="item in this.$store.getters.getSearchList"
+          :key="item.id"
+        >
+          {{ item.name }}
+        </div>
       </div>
     </div>
   </div>
@@ -43,6 +52,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.catalog_block_content_links {
+  list-style: none;
+  display: flex;
+  text-align: left;
+  align-items: center;
+  flex-direction: column;
+  width: 315px;
+}
+.catalog_block_items {
+  display: flex;
+  justify-content: space-between;
+}
+.block_content_link {
+  padding: 20px 16px 20px 30px;
+  width: 100%;
+  margin-bottom: 5px;
+  background: #f8f8f8 url("@/assets/catalogLinkIcon.svg") no-repeat;
+  background-position: 2% 50%;
+  border-radius: 4px;
+}
+.block_content_link,
+text {
+  font-family: Helvetica;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0em;
+  text-align: left;
+}
 .catalog_block_header_text {
   margin: 50px 0;
   color: var(--black-base, #2c2c2c);
